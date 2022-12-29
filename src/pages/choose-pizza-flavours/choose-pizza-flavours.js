fc263dc7fc7c15b8d36c492cbaa5d9d8c6648bda
Guisegtrab <88219289+Guisegtrab@users.noreply.github.com>
2022-12-28 21:53:14 -0300

    Implementação Footer OrderContainer dados user and order

--- src/pages/choose-pizza-flavours/choose-pizza-flavours.js
+++ src/pages/choose-pizza-flavours/choose-pizza-flavours.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { AuthContext } from 'contexts/auth'
 import t from 'prop-types'
 import styled from 'styled-components'
 import { Redirect } from 'react-router-dom'
 import {
   Card as MaterialCard,
+  Container,
   Grid,
   Typography
 } from '@material-ui/core'
@@ -12,7 +14,8 @@ import {
   Divider,
   H4,
   HeaderContent,
-  PizzasGrid
+  PizzasGrid,
+  Content
 } from 'ui'
 import { singularOrPlural, toMoney } from 'utils'
 import { HOME } from 'routes'
@@ -21,12 +24,14 @@ import pizzasFlavours from 'fake-data/pizzas-flavours.js'
 
 const ChoosePizzaFlavours = ({ location }) => {
   const [checkboxes, setCheckboxes] = useState(() => ({}))
+  const { userInfo } = useContext(AuthContext)
+  console.log('userInfo', userInfo)
 
   if (!location.state) {
     return <Redirect to={HOME} />
   }
 
-  const { flavours, id } = location.state
+  const { flavours, id, name, slices } = location.state
 
   const handleChangeCheckbox = (pizzaId) => (e) => {
     console.log('checkboxes', checkboxes)
@@ -46,7 +51,7 @@ const ChoosePizzaFlavours = ({ location }) => {
   }
 
   return (
-    <>
+    <Content>
       <HeaderContent>
         <H4>
           Escolha até {flavours} {' '}
@@ -77,7 +82,28 @@ const ChoosePizzaFlavours = ({ location }) => {
           </Grid>
         ))}
       </PizzasGrid>
-    </>
+
+      <Footer>
+        <Container>
+          <Grid container>
+            <OrderContainer>
+              <typography>
+                <b>{userInfo.user.email}, seu pedido é: </b>
+              </typography>
+
+              <typography>
+                <b>Pizza <b>{name.toUpperCase()}</b> { '-' }
+                {slices} {singularOrPlural(slices, 'fatia', 'fatias')}, {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')}</b>
+              </typography>
+            </OrderContainer>
+
+            <Grid item>
+              Botão
+            </Grid>
+          </Grid>
+        </Container>
+      </Footer>
+    </Content>
   )
 }
 
@@ -90,9 +116,10 @@ function checkboxesChecked (checkboxes) {
 }
 
 const Card = styled(MaterialCard)`
-  border: 2px solid transparent;
-  border-color: ${({ theme, checked }) => checked ? theme.palette.secondary.light : ''};
-`
+  && {
+    border: 2px solid transparent;
+    border-color: ${({ theme, checked }) => checked ? theme.palette.secondary.light : ''};
+  }`
 
 const Label = styled(CardLink).attrs({
   component: 'label'
@@ -101,11 +128,25 @@ const Label = styled(CardLink).attrs({
 const Checkbox = styled.input.attrs({
   type: 'checkbox'
 })`
-  display: none;
-`
+      display: none;
+    `
 
 const Img = styled.img`
-  width: 200px;
+      width: 200px;
+    `
+
+const Footer = styled.footer`
+      box-shadow: ${({ theme }) => theme.palette.grey[400]};
+      padding: ${({ theme }) => theme.spacing(3)}px;
+      width: 100%;
+    `
+
+const OrderContainer = styled(Grid).attrs({
+  item: true
+})`
+  && {
+    flex-grow: 1;
+ }
 `
 
 export default ChoosePizzaFlavours
